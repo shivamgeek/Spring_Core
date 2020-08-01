4e24982ebad97eff008d3682fc2bdafec89bc495
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-02 00:11:17 +0530

    Autowiring

--- drawing_shapes/src/main/java/com/shivam/drawing_shapes/Triangle.java
+++ drawing_shapes/src/main/java/com/shivam/drawing_shapes/Triangle.java
@@ -1,26 +1,37 @@
 package com.shivam.drawing_shapes;
 
-import java.util.List;
-
 public class Triangle implements Shape {
 
-	private List<Point> points;	
+	private Point pointA_variable, pointB_variable, pointC_variable; 
 
-
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA_variable() {
+		return pointA_variable;
 	}
 
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA_variable(Point pointA_variable) {
+		this.pointA_variable = pointA_variable;
 	}
 
+
+	public Point getPointB_variable() {
+		return pointB_variable;
+	}
+
+	public void setPointB_variable(Point pointB_variable) {
+		this.pointB_variable = pointB_variable;
+	}
+
+	public Point getPointC_variable() {
+		return pointC_variable;
+	}
+
+	public void setPointC_variable(Point pointC_variable) {
+		this.pointC_variable = pointC_variable;
+	}
 
 	public void draw() {
-		for(Point p : points) {
-			System.out.println("Drawing shape Triangle, points are "+p);
-		}
+		System.out.println("Drawing shape Triangle, points are "+pointA_variable+", "+pointB_variable+", "+pointC_variable);
 	}
 	
 }
