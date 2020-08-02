436a324fc3a63b290262f6c1a2265ff189d827ef
Shivam Aggarwal <shivam01101995@gmail.com>
2020-08-02 18:21:22 +0530

    Final

--- firstApp/src/main/java/com/shivam/firstApp/Car.java
+++
@@ -1,7 +0,0 @@
-package com.shivam.firstApp;
-
-public interface Car {
-	
-	void driveCar();
-	
-}
--- firstApp/src/main/java/com/shivam/firstApp/Maruti.java
+++
@@ -1,9 +0,0 @@
-package com.shivam.firstApp;
-
-public class Maruti implements Car {
-
-	public void driveCar() {
-		System.out.println("Driving Maruti car");
-	}
-	
-}
